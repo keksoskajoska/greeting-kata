be44a9b7829b8b76841afd9f4625102b49ce8e9c
András Bogár-Szabó <bszandras@gmail.com>
2018-02-28 10:56:17 +0100

    elfelejtettem pusholni eddig

--- good morning/main.cpp
+++ good morning/main.cpp
@@ -1,30 +1,65 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
 
 using namespace std;
 string name;
+string text;
 
 void GetName()
 {
     cin>>name;
+    if(name == "-"){
+        name = "my friend";
+    }
     cout<<"\n\n\n"<<endl;
-}
-void Cout()
-{
-    cout << "\nHappy Eastern, " << name << "!" << endl;
 }
 void Mexico()
 {
-    for(int i=0;i<81;++i)
+    for(int i=0;i<80;++i)
     {
         cout<<"*";
     }
 }
+void String()
+{
+    text = "Happy Eastern, " + name + "!";
+}
+void centerstring()
+{
+    int l = text.length();
+    int FrontSpaces = (78 - l)/2;
+    cout<<"*";
+    for(int i=0; i<FrontSpaces;i++)
+    {
+        cout<<" ";
+    }
+    cout<<text;
+    for(int i=0; i<FrontSpaces;i++)
+    {
+        cout<<" ";
+    }
+    cout<<"*";
+
+}
+
+void Hungary()
+{
+    cout<<"*";
+    for(int i=0;i<78;i++)
+    {
+        cout<<" ";
+    }
+    cout<<"*";
+}
 int main()
 {
     GetName();
+    String();
     Mexico();
-    Cout();
+    Hungary();
+    centerstring();
+    Hungary();
     Mexico();
     cout<<"\n\n\n"<<endl;
     return 0;
