a4d37cbd62eade631958df6af5f990d18a0261f6
András Bogár-Szabó <bszandras@gmail.com>
2018-02-28 10:07:10 +0100

    greeting step 4

--- good morning/main.cpp
+++ good morning/main.cpp
@@ -7,14 +7,25 @@ string name;
 void GetName()
 {
     cin>>name;
+    cout<<"\n\n\n"<<endl;
 }
 void Cout()
 {
-    cout << "Happy Eastern, " << name << "!" << endl;
+    cout << "\nHappy Eastern, " << name << "!" << endl;
+}
+void Mexico()
+{
+    for(int i=0;i<81;++i)
+    {
+        cout<<"*";
+    }
 }
 int main()
 {
     GetName();
+    Mexico();
     Cout();
+    Mexico();
+    cout<<"\n\n\n"<<endl;
     return 0;
 }
